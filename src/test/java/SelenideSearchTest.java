9e3e042719be687e17dcb71ed3f1ef5137678985
TnBreeze <ytka0509@gmaol>
2024-09-15 21:47:27 +0300

    added SelenideSearchTest

---
+++ src/test/java/SelenideSearchTest.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideSearchTest {
+    @BeforeAll
+    static void beforeall() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.reopenBrowserOnFail = false;
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void SelenideWikiTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body ul").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+        
+                """));
+    }
+}
